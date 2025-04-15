0110e40d350bf85f32c1b9911cca73e73187f4bd
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-14 23:37:12 -0300

    Update 1183-Acima-da-Diagonal-Principal.py

--- 1183-Acima-da-Diagonal-Principal.py
+++ 1183-Acima-da-Diagonal-Principal.py
@@ -2,23 +2,21 @@ O = input()
 
 diagonal = []
 
-for i in range (3):
+for i in range (0,12):
     lista = []
-    for j in range(3): 
+    for j in range(0,12): 
         numero = float(input())
         lista.append(numero)
     diagonal.append(lista)
 
 resultado = 0
-contador = 0
 
 for linha in range (len(diagonal)):
     for coluna in range (len(diagonal)):
-        coluna = 0
-        colunas = coluna + 1
-        resultado += diagonal[linha][colunas]
+        if (linha<coluna):
+            resultado+= diagonal[linha][coluna]
 
 if O =="M":
-    resultado/= contador
+    resultado/= 66
 
 print(f'{resultado:.1f}')
